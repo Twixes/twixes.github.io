0449cab6bef250d0622260cae9665b9f6353e370
Twixes <dev@twixes.com>
2019-06-10 18:41:51 +0200

    Add website files

---
+++ errors/502.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/snippets/author.php") ?>
+  <head>
+    <meta charset="utf-8">
+    <title>Twixes</title>
+    <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/snippets/head.php") ?>
+    <link rel="stylesheet" href="/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:500i|Roboto+Mono:500i">
+  </head>
+  <body>
+    <div class="error-code">502</div>
+    <div class="destinations-list">
+      <a href="http://twixes.com"><span class="destination-home">Twixes</span></a>
+    </div>
+    <script src="/main.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+  <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/snippets/author.php") ?>
+  <head>
+    <meta charset="utf-8">
+    <title>Twixes</title>
+    <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/snippets/head.php") ?>
+    <link rel="canonical" href="https://twixes.com/">
+    <link rel="stylesheet" href="/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,500i">
+  </head>
+  <body>
+    <div class="t">T</div>
+    <ul class="destinations-list">
+      <li><a href="https://github.com/Twixes">GitHub</a></li>
+      <span class="destinations-list-separator"></span>
+      <li><a href="http://codepen.io/Twixes">CodePen</a></li>
+      <span class="destinations-list-separator"></span>
+      <li><a href="https://steamcommunity.com/id/Twixes">Steam</a></li>
+      <span class="destinations-list-separator"></span>
+      <li><a href="mailto:hello@twixes.com">hello@twixes.com</a></li>
+    </ul>
+    <script src="/main.js"></script>
+  </body>
+</html>
