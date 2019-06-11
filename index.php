8feebf01108528c5e09950474ef2ffd2d03fb2e4
Twixes <dev@twixes.com>
2019-06-11 02:27:59 +0200

    Remove Steam from destinations

--- index.php
+++ index.php
@@ -16,8 +16,6 @@
       <span class="destinations-list__separator"></span>
       <li><a href="http://codepen.io/Twixes">CodePen</a></li>
       <span class="destinations-list__separator"></span>
-      <li><a href="https://steamcommunity.com/id/Twixes">Steam</a></li>
-      <span class="destinations-list__separator"></span>
       <li><a href="mailto:hello@twixes.com">hello@twixes.com</a></li>
     </ul>
     <script src="/main.js"></script>
