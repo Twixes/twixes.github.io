7e470388c6cdb6de6f5cd03bdafcb6045bd7f0f6
Twixes <dev@twixes.com>
2020-05-16 02:15:49 +0200

    Remove remnants of PHP

--- snippets/head.php
+++
@@ -1,9 +0,0 @@
-<meta name="description" content="I'm Twixes.">
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-<meta name="theme-color" content="#ff3700">
-<link rel="shortcut icon" href="/favicon.ico">
-<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#ff3700">
